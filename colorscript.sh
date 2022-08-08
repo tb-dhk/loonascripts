11ae26199fd2dd1dfda12bca3ec4e6499fc31820
shua <86475694+shuu-wasseo@users.noreply.github.com>
2022-08-08 18:37:12 +0800

    Update colorscript.sh

--- colorscript.sh
+++ colorscript.sh
@@ -1,11 +1,27 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 # Simple CLI for shell-color-scripts
 
-DIR_COLORSCRIPTS="/opt/shell-color-scripts/colorscripts"
+if [[ "$DEV" -gt 0 ]]; then
+    DIR_COLORSCRIPTS="./colorscripts"
+else
+    DIR_COLORSCRIPTS="/opt/shell-color-scripts/colorscripts"
+fi
+
+if command -v find &>/dev/null; then
+    LS_CMD="$(command -v find) ${DIR_COLORSCRIPTS} -maxdepth 1 -type f"
+    LS_CMD_B="$(command -v find) ${DIR_COLORSCRIPTS}/blacklisted -maxdepth 1 -type f"
+else
+    LS_CMD="$(command -v ls) ${DIR_COLORSCRIPTS}"
+    LS_CMD_B="$(command -v ls) ${DIR_COLORSCRIPTS}/blacklisted"
+fi
+
+list_colorscripts="$($LS_CMD | xargs -I $ basename $ | cut -d ' ' -f 1 | nl)"
+length_colorscripts="$($LS_CMD | wc -l)"
+list_blacklist="$($LS_CMD_B 2>/dev/null | xargs -I $ basename $ | cut -d ' ' -f 1 | nl || "")" 
+length_blacklist="$($LS_CMD_B 2>/dev/null | wc -l || 0)"
+
 fmt_help="  %-20s\t%-54s\n"
-list_colorscripts="$(/usr/bin/ls "${DIR_COLORSCRIPTS}" | cut -d ' ' -f 1 | nl)"
-length_colorscripts="$(/usr/bin/ls "${DIR_COLORSCRIPTS}" | wc -l)"
 function _help() {
     echo "Description: A collection of terminal color scripts."
     echo ""
@@ -14,12 +30,20 @@ function _help() {
         "-h, --help, help" "Print this help." \
         "-l, --list, list" "List all installed color scripts." \
         "-r, --random, random" "Run a random color script." \
-        "-e, --exec, exec" "Run a specified color script by SCRIPT NAME or INDEX."
+        "-e, --exec, exec" "Run a specified color script by SCRIPT NAME or INDEX."\
+        "-b, --blacklist, blacklist" "Blacklist a color script by SCRIPT NAME or INDEX." \
+        "-u, --unblacklist, unblacklist" "Unblacklist a color script by SCRIPT NAME or INDEX." \
+        "-a, --all, all" "List the outputs of all colorscripts with their SCRIPT NAME"
 }
 
 function _list() {
-    echo "There are "$(/usr/bin/ls "${DIR_COLORSCRIPTS}" | wc -l)" installed color scripts:"
+    echo "There are "$($LS_CMD | wc -l)" installed color scripts:"
     echo "${list_colorscripts}"
+}
+
+function _list_blacklist() {
+    echo "There are $length_blacklist blacklisted color scripts:"
+    echo "${list_blacklist}"
 }
 
 function _random() {
@@ -28,7 +52,7 @@ function _random() {
 
     random_colorscript="$(echo  "${list_colorscripts}" | sed -n ${random_index}p \
         | tr -d ' ' | tr '\t' ' ' | cut -d ' ' -f 2)"
-    echo "${random_colorscript}"
+    # echo "${random_colorscript}"
     exec "${DIR_COLORSCRIPTS}/${random_colorscript}"
 }
 
@@ -48,7 +72,7 @@ function _run_by_name() {
 }
 
 function _run_by_index() {
-    if [[ "$1" -gt 0 && "$1" -lt "${length_colorscripts}" ]]; then
+    if [[ "$1" -gt 0 && "$1" -le "${length_colorscripts}" ]]; then
 
         colorscript="$(echo  "${list_colorscripts}" | sed -n ${1}p \
             | tr -d ' ' | tr '\t' ' ' | cut -d ' ' -f 2)"
@@ -67,6 +91,30 @@ function _run_colorscript() {
     fi
 }
 
+function _run_all() {
+    for s in $DIR_COLORSCRIPTS/*
+    do
+        echo "$(echo $s | awk -F '/' '{print $NF}'):"
+        echo "$($s)"
+        echo
+    done
+}
+
+function _blacklist_colorscript() { # by name only
+    if [ ! -d "${DIR_COLORSCRIPTS}/blacklisted" ]; then
+        sudo mkdir "${DIR_COLORSCRIPTS}/blacklisted"
+    fi
+    sudo mv "${DIR_COLORSCRIPTS}/$1" "${DIR_COLORSCRIPTS}/blacklisted"
+}
+
+function _unblacklist_colorscript() { # by name only
+    if [ -f "${DIR_COLORSCRIPTS}/blacklisted/$1" ]; then
+        sudo mv "${DIR_COLORSCRIPTS}/blacklisted/$1" "${DIR_COLORSCRIPTS}"
+    else
+        echo "Input error. Script $1 is not blacklisted!"
+    fi
+}
+
 case "$#" in
     0)
         _help
@@ -79,8 +127,14 @@ case "$#" in
             -l | --list | list)
                 _list
                 ;;
+            -b | --blacklist | blacklist)
+                _list_blacklist
+                ;;
             -r | --random | random)
                 _random
+                ;;
+            -a | --all | all)
+                _run_all
                 ;;
             *)
                 echo "Input error."
@@ -91,6 +145,10 @@ case "$#" in
     2)
         if [[ "$1" == "-e" || "$1" == "--exec" || "$1" == "exec" ]]; then
             _run_colorscript "$2"
+        elif [[ "$1" == "-b" || "$1" == "--blacklist" || "$1" == "blacklist" ]]; then
+            _blacklist_colorscript "$2"
+        elif [[ "$1" == "-u" || "$1" == "--unblacklist" || "$1" == "unblacklist" ]]; then
+            _unblacklist_colorscript "$2"
         else
             echo "Input error."
             exit 1
