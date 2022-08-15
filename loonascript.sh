b611d0748b392ac260cc1cc666ba798f175b04d4
shuu-wasseo <joshuasoh2008@gmail.com>
2022-08-15 18:40:56 +0700

    yes

--- loonascript.sh
+++ loonascript.sh
@@ -2,7 +2,7 @@
 
 # Simple CLI for shell-color-scripts
 
-DIR_COLORSCRIPTS="/opt/shell-color-scripts/colorscripts"
+DIR_COLORSCRIPTS="/home/shua/loonascripts/colorscripts"
 fmt_help="  %-20s\t%-54s\n"
 list_colorscripts="$(/usr/bin/ls "${DIR_COLORSCRIPTS}" | cut -d ' ' -f 1 | nl)"
 length_colorscripts="$(/usr/bin/ls "${DIR_COLORSCRIPTS}" | wc -l)"
