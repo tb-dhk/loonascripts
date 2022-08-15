cb5b04d88fe694f8f59ea902c888eb6c9db499c9
shua <86475694+shuu-wasseo@users.noreply.github.com>
2022-08-16 07:16:37 +0800

    Create initialise.sh

---
+++ colorscripts/initialise.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+initializeANSI()
+{
+  esc=""
+
+  heejf="${esc}[38;2;236;0;140m";
+  hyunf="${esc}[38;2;255;204;0m";
+  hasef="${esc}[38;2;0;166;81m";
+  yeojf="${esc}[38;2;244;111;31m";
+  vivif="${esc}[38;2;225;152;180m";
+  kimlf="${esc}[38;2;228;26;62m";
+  jinsf="${esc}[38;2;20;36;176m"; 
+  choef="${esc}[38;2;92;44;146m";
+  yvesf="${esc}[38;2;126;0;47m";
+  chuuf="${esc}[38;2;246;144;126m";
+  gowof="${esc}[38;2;48;195;156m";
+  olivf="${esc}[38;2;143;143;143m";
+
+  boldon="${esc}[1m";    boldoff="${esc}[22m"
+  italicson="${esc}[3m"; italicsoff="${esc}[23m"
+  ulon="${esc}[4m";      uloff="${esc}[24m"
+  invon="${esc}[7m";     invoff="${esc}[27m"
+
+  reset="${esc}[0m"
+}
