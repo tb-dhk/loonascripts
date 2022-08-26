6320c0071cad810507b899fe44ff0f2ecce32766
shua <86475694+shuu-wasseo@users.noreply.github.com>
2022-08-26 16:04:32 +0800

    Update initialise.sh

--- colorscripts/initialise.sh
+++ colorscripts/initialise.sh
@@ -16,10 +16,26 @@ initializeANSI()
   gowof="${esc}[38;2;48;195;156m";
   olivf="${esc}[38;2;143;143;143m";
 
+  heejb="${esc}[48;2;236;0;140m";
+  hyunb="${esc}[48;2;255;204;0m";
+  haseb="${esc}[48;2;0;166;81m";
+  yeojb="${esc}[48;2;244;111;31m";
+  vivib="${esc}[48;2;225;152;180m";
+  kimlb="${esc}[48;2;228;26;62m";
+  jinsb="${esc}[48;2;20;36;176m"; 
+  choeb="${esc}[48;2;92;44;146m";
+  yvesb="${esc}[48;2;126;0;47m";
+  chuub="${esc}[48;2;246;144;126m";
+  gowob="${esc}[48;2;48;195;156m";
+  olivb="${esc}[48;2;143;143;143m";
+
   boldon="${esc}[1m";    boldoff="${esc}[22m"
   italicson="${esc}[3m"; italicsoff="${esc}[23m"
   ulon="${esc}[4m";      uloff="${esc}[24m"
   invon="${esc}[7m";     invoff="${esc}[27m"
+  
+  reset="${esc}[0m";
 
-  reset="${esc}[0m"
+  Bf="${esc}[30m"
 }
+
